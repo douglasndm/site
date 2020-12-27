2acada4af87abfa6d32f3ce6bdbe5ab31ae4295e
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-27 17:19:26 -0300

    fix: styled for scroll view

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
-    position: absolute;
+    position: fixed;
     width: 100vw;
 `;
 
