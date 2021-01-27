888be982c596954100e6b9ec7b8bb5503dcde70f
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-01-27 18:02:37 -0300

    Updating terms

--- src/Pages/Terms/index.tsx
+++ src/Pages/Terms/index.tsx
@@ -9,24 +9,32 @@ const Terms: React.FC = () => (
         <Header fixed={false} />
         <PrivacyContainer>
             <h1>Termos de uso</h1>
+            <br />
+            <br />
             <h2>1. Termos</h2>
             <p>
-                Ao acessar ao site{' '}
-                <a href="https://douglasndm.dev">douglasndm</a>, concorda em
-                cumprir estes termos de serviço, todas as leis e regulamentos
-                aplicáveis e concorda que é responsável pelo cumprimento de
-                todas as leis locais aplicáveis. Se você não concordar com algum
-                desses termos, está proibido de usar ou acessar este site. Os
-                materiais contidos neste site são protegidos pelas leis de
-                direitos autorais e marcas comerciais aplicáveis.
+                Ao acessar ao site e/ou aplicativo de{' '}
+                <a href="https://douglasndm.dev">
+                    Douglas Nunes de Mattos (douglasndm)
+                </a>
+                , concorda em cumprir estes termos de serviço, todas as leis e
+                regulamentos aplicáveis e concorda que é responsável pelo
+                cumprimento de todas as leis locais aplicáveis. Se você não
+                concordar com algum desses termos, está proibido de usar ou
+                acessar este site e/ou aplicativo. Os materiais contidos neste
+                site e/ou aplicativo são protegidos pelas leis de direitos
+                autorais e marcas comerciais aplicáveis.
             </p>
+            <br />
+            <br />
             <h2>2. Uso de Licença</h2>
             <p>
                 É concedida permissão para baixar temporariamente uma cópia dos
-                materiais (informações ou software) no site douglasndm , apenas
-                para visualização transitória pessoal e não comercial. Esta é a
-                concessão de uma licença, não uma transferência de título e, sob
-                esta licença, você não pode:
+                materiais (informações ou software) no site e/ou aplicativo de
+                Douglas Nunes de Mattos (douglasndm), apenas para visualização
+                transitória pessoal e não comercial. Esta é a concessão de uma
+                licença, não uma transferência de título e, sob esta licença,
+                você não pode:
             </p>
             <ol>
                 <li>modificar ou copiar os materiais;</li>
@@ -36,7 +44,8 @@ const Terms: React.FC = () => (
                 </li>
                 <li>
                     tentar descompilar ou fazer engenharia reversa de qualquer
-                    software contido no site douglasndm;
+                    software contido no site e/ou aplicativo de Douglas Nunes de
+                    Mattos (douglasndm);
                 </li>
                 <li>
                     remover quaisquer direitos autorais ou outras notações de
@@ -49,74 +58,90 @@ const Terms: React.FC = () => (
             </ol>
             <p>
                 Esta licença será automaticamente rescindida se você violar
-                alguma dessas restrições e poderá ser rescindida por douglasndm
-                a qualquer momento. Ao encerrar a visualização desses materiais
-                ou após o término desta licença, você deve apagar todos os
-                materiais baixados em sua posse, seja em formato eletrónico ou
-                impresso.
+                alguma dessas restrições e poderá ser rescindida por Douglas
+                Nunes de Mattos (douglasndm) a qualquer momento. Ao encerrar a
+                visualização desses materiais ou após o término desta licença,
+                você deve apagar todos os materiais baixados em sua posse, seja
+                em formato eletrónico ou impresso.
             </p>
+            <br />
+            <br />
             <h2>3. Isenção de responsabilidade</h2>
             <ol>
                 <li>
-                    Os materiais no site da douglasndm são fornecidos &apos;como
-                    estão&apos;. douglasndm não oferece garantias, expressas ou
-                    implícitas, e, por este meio, isenta e nega todas as outras
-                    garantias, incluindo, sem limitação, garantias implícitas ou
-                    condições de comercialização, adequação a um fim específico
-                    ou não violação de propriedade intelectual ou outra violação
-                    de direitos.
+                    Os materiais no site e/ou aplicativo de Douglas Nunes de
+                    Mattos (douglasndm) são fornecidos &apos;como estão&apos;.
+                    Douglas Nunes de Mattos (douglasndm) não oferece garantias,
+                    expressas ou implícitas, e, por este meio, isenta e nega
+                    todas as outras garantias, incluindo, sem limitação,
+                    garantias implícitas ou condições de comercialização,
+                    adequação a um fim específico ou não violação de propriedade
+                    intelectual ou outra violação de direitos.
                 </li>
                 <li>
-                    Além disso, o douglasndm não garante ou faz qualquer
-                    representação relativa à precisão, aos resultados prováveis
-                    ou à confiabilidade do uso dos materiais em seu site ou de
-                    outra forma relacionado a esses materiais ou em sites
-                    vinculados a este site.
+                    Além disso, o Douglas Nunes de Mattos (douglasndm) não
+                    garante ou faz qualquer representação relativa à precisão,
+                    aos resultados prováveis ou à confiabilidade do uso dos
+                    materiais em seu site e/ou aplicativo ou de outra forma
+                    relacionado a esses materiais ou em site e/ou aplicativos
+                    vinculados a este site e/ou aplicativo.
                 </li>
             </ol>
+            <br />
+            <br />
             <h2>4. Limitações</h2>
             <p>
-                Em nenhum caso o douglasndm ou seus fornecedores serão
-                responsáveis por quaisquer danos (incluindo, sem limitação,
-                danos por perda de dados ou lucro ou devido a interrupção dos
-                negócios) decorrentes do uso ou da incapacidade de usar os
-                materiais em douglasndm, mesmo que douglasndm ou um
-                representante autorizado da douglasndm tenha sido notificado
-                oralmente ou por escrito da possibilidade de tais danos. Como
-                algumas jurisdições não permitem limitações em garantias
-                implícitas, ou limitações de responsabilidade por danos
-                conseqüentes ou incidentais, essas limitações podem não se
-                aplicar a você.
+                Em nenhum caso o Douglas Nunes de Mattos (douglasndm) ou seus
+                fornecedores serão responsáveis por quaisquer danos (incluindo,
+                sem limitação, danos por perda de dados ou lucro ou devido a
+                interrupção dos negócios) decorrentes do uso ou da incapacidade
+                de usar os materiais em site e/ou aplicativos de Douglas Nunes
+                de Mattos (douglasndm), mesmo que Douglas Nunes de Mattos
+                (douglasndm) ou um representante autorizado da Douglas Nunes de
+                Mattos (douglasndm) tenha sido notificado oralmente ou por
+                escrito da possibilidade de tais danos. Como algumas jurisdições
+                não permitem limitações em garantias implícitas, ou limitações
+                de responsabilidade por danos conseqüentes ou incidentais, essas
+                limitações podem não se aplicar a você.
             </p>
+            <br />
+            <br />
             <h2>5. Precisão dos materiais</h2>
             <p>
-                Os materiais exibidos no site da douglasndm podem incluir erros
-                técnicos, tipográficos ou fotográficos. douglasndm não garante
-                que qualquer material em seu site seja preciso, completo ou
-                atual. douglasndm pode fazer alterações nos materiais contidos
-                em seu site a qualquer momento, sem aviso prévio. No entanto,
-                douglasndm não se compromete a atualizar os materiais.
+                Os materiais exibidos no site e/ou aplicativo de Douglas Nunes
+                de Mattos (douglasndm) podem incluir erros técnicos,
+                tipográficos ou fotográficos. Douglas Nunes de Mattos
+                (douglasndm) não garante que qualquer material em seu site e/ou
+                aplicativo seja preciso, completo ou atual. Douglas Nunes de
+                Mattos (douglasndm) pode fazer alterações nos materiais contidos
+                em seu site e/ou aplicativo a qualquer momento, sem aviso
+                prévio. No entanto, Douglas Nunes de Mattos (douglasndm) não se
+                compromete a atualizar os materiais.
             </p>
+            <br />
+            <br />
             <h2>6. Links</h2>
             <p>
-                O douglasndm não analisou todos os sites vinculados ao seu site
-                e não é responsável pelo conteúdo de nenhum site vinculado. A
-                inclusão de qualquer link não implica endosso por douglasndm do
-                site. O uso de qualquer site vinculado é por conta e risco do
-                usuário.
+                O Douglas Nunes de Mattos (douglasndm) não analisou todos os
+                sites vinculados ao seu site e/ou aplicativo e não é responsável
+                pelo conteúdo de nenhum site vinculado. A inclusão de qualquer
+                link não implica endosso por Douglas Nunes de Mattos
+                (douglasndm) do site. O uso de qualquer site e/ou aplicativo
+                vinculado é por conta e risco do usuário.
             </p>
             <h3>Modificações</h3>
             <p>
-                O douglasndm pode revisar estes termos de serviço do site a
-                qualquer momento, sem aviso prévio. Ao usar este site, você
-                concorda em ficar vinculado à versão atual desses termos de
-                serviço.
+                O Douglas Nunes de Mattos (douglasndm) pode revisar estes termos
+                de serviço do site e/ou aplicativo a qualquer momento, sem aviso
+                prévio. Ao usar este site e/ou aplicativo, você concorda em
+                ficar vinculado à versão atual desses termos de serviço.
             </p>
             <h3>Lei aplicável</h3>
             <p>
                 Estes termos e condições são regidos e interpretados de acordo
-                com as leis do douglasndm e você se submete irrevogavelmente à
-                jurisdição exclusiva dos tribunais naquele estado ou localidade.
+                com as leis de Douglas Nunes de Mattos (douglasndm) e você se
+                submete irrevogavelmente à jurisdição exclusiva dos tribunais
+                naquele estado ou localidade.
             </p>
         </PrivacyContainer>
     </Container>
