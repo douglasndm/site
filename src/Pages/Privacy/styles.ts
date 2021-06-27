a3cb7b1b36a074f082cbd4429189d86f8b31b3ef
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-06-27 16:26:02 -0300

    fix: Header on Privacy/Terms

--- src/Pages/Privacy/styles.ts
+++ src/Pages/Privacy/styles.ts
@@ -10,4 +10,18 @@ export const PrivacyContainer = styled.div`
 
     background: white;
     color: black;
+
+    @media (max-width: 800px) {
+        margin: 11% 0;
+        padding: 6% 5%;
+    }
+
+    @media (max-width: 400px) {
+        margin: 15% 0;
+        padding: 6% 5%;
+    }
+
+    @media (max-width: 290px) {
+        margin: 0;
+    }
 `;
